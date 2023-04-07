22f2465aafb4705868221d64df3d798c553bf0f2
vitor silva <vitorsilvadeoliveira050@gmail.com>
2023-04-06 22:04:19 -0300

    aulaangular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListarProdutosComponent } from './components/produto/listar-produtos/listar-produtos.component';
+import { HomeComponent } from './template/home/home.component';
+import { CadastrarProdutoComponent } from './components/produto/listar-produtos/cadastrar-produto/cadastrar-produto.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'produtos', component:ListarProdutosComponent},
+  {path:'produtos/cadastrar', component:CadastrarProdutoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/produto/listar-produtos/listar-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-produtos',
+  templateUrl: './listar-produtos.component.html',
+  styleUrls: ['./listar-produtos.component.css']
+})
+export class ListarProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
